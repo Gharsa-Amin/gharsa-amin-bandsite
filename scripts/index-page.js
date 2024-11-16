a7fc4819f9028f6134e744f031e59a120e76a5dc
Gharsa Amin <ghars0718@gmail.com>
2024-11-15 23:57:45 -0500

    worked on mobile first approach for responsive designing

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,21 +3,21 @@ const commentSection = [
 		Name: "Victor Pinto",
 		DateTime: "11/02/2023",
 		Comment:
-		"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+			"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
 	},
 
 	{
 		Name: "Christina Cabrera",
 		DateTime: "10/28/2023",
 		Comment:
-		"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+			"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
 	},
 
 	{
 		Name: "Isaac Tadesse",
 		DateTime: "10/20/2023",
 		Comment:
-		"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+			"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
 	},
 ];
 
@@ -25,13 +25,12 @@ function renderComment(comment) {
 	const li = document.createElement("li");
 	li.classList.add("comment-section");
 
+	const wrapper = document.createElement("div");
+	wrapper.classList.add("wrapper");
 
-	const wrapper = document.createElement("div"); 
-	wrapper.classList.add("wrapper"); 
+	const avatarClass = document.createElement("article");
+	avatarClass.classList.add("wrapper__avatar");
 
-	const avatarClass = document.createElement("article"); 
-    avatarClass.classList.add("wrapper__avatar"); 
-    
 	const name = document.createElement("p");
 	name.classList.add("wrapper__name");
 	name.textContent = comment.Name;
@@ -40,16 +39,21 @@ function renderComment(comment) {
 	dateTime.classList.add("wrapper__dateTime");
 	dateTime.textContent = comment.DateTime;
 
+	const commentWrapper = document.createElement("article"); 
+	commentWrapper.classList.add("comment-wrapper"); 
+	
 	const commentPart = document.createElement("p");
-	commentPart.classList.add("comment-section__comment");
+	commentPart.classList.add("comment-wrapper__comment");
 	commentPart.textContent = comment.Comment;
 
 
-	li.appendChild(wrapper)
-    wrapper.appendChild(avatarClass); 
+	li.appendChild(wrapper);
+	wrapper.appendChild(avatarClass);
 	wrapper.appendChild(name);
 	wrapper.appendChild(dateTime);
 	li.appendChild(commentPart);
+	commentWrapper.appendChild(commentPart); 
+	li.appendChild(commentWrapper); 
 
 	const dynamicContent = document.getElementById("dynamicContent");
 	dynamicContent.prepend(li);
@@ -70,6 +74,10 @@ const handleSubmit = (event) => {
 	const name = document.getElementById("itemInput").value;
 	const commentPart = document.getElementById("itemComment").value;
 
+	function sortComments(comments) {
+		return comments.sort((a, b) => new Date(b.DateTime) - new Date(a.DateTime));
+	}
+
 	if (name && commentPart) {
 		const newComment = {
 			Name: name,
@@ -77,28 +85,18 @@ const handleSubmit = (event) => {
 			Comment: commentPart,
 		};
 
-		commentSection.unshift(newComment); // It would be useful to add an external API to make sure that the comments are in meaningful langauge rather than just Gibberish langauge, but it wasnt part of the assignement requirment so I didnt add it. 
+		commentSection.unshift(newComment); // It would be useful to add an external API to make sure that the comments are in meaningful langauge rather than just Gibberish langauge, but it wasnt part of the assignement requirment so I didnt add it.
 
 		document.getElementById("itemInput").value = "";
 		document.getElementById("itemComment").value = "";
 
-		renderComment(newComment); 
+		renderComment(newComment);
+	} else {
+		alert("Please fill in your name and comment.");
 	}
-		else  {
-			alert("Please fill in your name and comment.")
-		}
 };
 
 formEl.addEventListener("submit", handleSubmit);
 
 renderAllComments();
-
-
-
-
-
-
-
-
-
 
