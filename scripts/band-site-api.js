f3094e395dff91246a2dbffc54e6530f6cfb1892
Gharsa Amin <ghars0718@gmail.com>
2024-11-19 22:10:39 -0500

    added postComment, getShows, and get Comments, and appended it to each of the API request URLs

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,25 +1,90 @@
 
-let apiKey= "2886777c-3308-49eb-af29-c97d7b690a3e"
+// let apiKey= "2886777c-3308-49eb-af29-c97d7b690a3e"
 
-class BandSiteApi  {
+ class BandSiteApi {
     constructor(apiKey) {
         this.apiKey = apiKey; 
-        this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
-    }
-    async postComment(comment) {
-        const url = `$(this.baseUrl)comments?api_key=${this.apiKey}`; 
+        this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/"; 
+    } 
+
+    async getComments() {
+        const url = `${this.baseUrl}comments?api_key=${this.apiKey}`;
+
         try {
-            const response = await axios.post(url, comment); 
-            return response.data
-            
+            const response = await axios.get(url);
+            const sortedComments = response.data.sort((a, b) => b.timestamp - a.timestamp);
+            console.log(response);
+            return sortedComments;
         } catch (error) {
-            console.log("Failed posting your comments")
-            
+            console.error("Failed to retrieve comments:", error);
         }
     }
-} 
+    async getShows() {
+        const url = `${this.baseUrl}showdates?api_key=${this.apiKey}`; 
+
+        try {
+            const response = await axios.get(url);
+            return response.data;  
+        } catch (error) {
+            console.error("Failed to retrieve shows:", error);
+        }
+    }
+    async postComment(comment) {
+        const url = `${this.baseUrl}comments?api_key=${this.apiKey}`; 
+
+        if (!comment.name || !comment.comment) {
+            throw new Error("Both 'name' and 'comment' are required.");
+        }
+
+        try {
+            const response = await axios.post(url, comment, {
+                headers: {
+                    "Content-Type": "application/json",  
+                },
+            });
+            return response.data; 
+        } catch (error) {
+            console.error("Failed to post comment:", error);
+        }
+    }
+}
 
 
 
+
+
+
+
+
+// class BandSiteApi  {
+//     constructor(apiKey) {
+//         this.apiKey = apiKey; 
+//         this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+//     }
+
+    
+//     async postComment(comment) {
+//         if (!comment.name || !comment.comment) {
+//             throw new Error("Both 'name' and 'comment' are required.");
+//         }
+
+//         const url = `$(this.baseUrl)comments?api_key=${this.apiKey}`; 
+//         try {
+//             const response = await axios.post(url, comment); {
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                 },
+//             });
+//             return response.data
+
+            
+//         } catch (error) {
+//             console.log("Failed posting your comments")
+            
+//         }
+//     }
+
+
+// }
 
 
